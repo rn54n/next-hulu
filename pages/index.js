d5b30fa387d1f9ec1b2714f26b897093eb860f9b
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-04-21 23:21:37 +0200

    test: add Results component and fetch data from api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <div>
       <Head>
@@ -17,6 +20,23 @@ export default function Home() {
       <Nav />
 
       {/* Results */}
+      <Results />
     </div>
   );
+}
+
+// will be executed before above render
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
