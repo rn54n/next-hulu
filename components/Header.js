b70a1259bd46600d1324e9b81dc27e92bfee69aa
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-04-21 21:56:33 +0200

    test: add logo and next config

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
     <header className="">
       <h1>This is the header</h1>
+      <Image
+        className="object-contain"
+        src="/assets/images/hulu-logo-white.png"
+        width={200}
+        height={100}
+      />
     </header>
   );
 };
