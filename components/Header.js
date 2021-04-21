6cd422b1c72d177fb8e59ead2dde148e5ada46a4
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-04-21 22:21:00 +0200

    test: add HeaderItem and icons

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <header className="">
-      <h1>This is the header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Trending" Icon={LightningBoltIcon} />
+        <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Collections" Icon={CollectionIcon} />
+        <HeaderItem title="Search" Icon={SearchIcon} />
+        <HeaderItem title="Account" Icon={UserIcon} />
+      </div>
       <Image
         className="object-contain"
         src="/assets/images/hulu-logo-white.png"
